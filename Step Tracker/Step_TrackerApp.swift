6e6aa8c80b3325319f35318c098f6f0803239457
jujubeyun <kre0708@naver.com>
2024-07-06 15:15:26 +0900

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Juhyun Yun on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
