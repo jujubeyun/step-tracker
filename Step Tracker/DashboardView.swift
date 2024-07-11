dc7806001a50b7c0ea8e179c97da2888bdf15a24
jujubeyun <kre0708@naver.com>
2024-07-11 15:20:42 +0900

    step bar charts styling

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -30,6 +30,12 @@ struct DashboardView: View {
     @State var selectedStat: HealthMetricContent = .steps
     var isSteps: Bool { selectedStat == .steps }
     
+    var avgStepCount: Double {
+        guard !hkManager.stepData.isEmpty else { return 0 }
+        let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
+        return totalSteps / Double(hkManager.stepData.count)
+    }
+    
     var body: some View {
         NavigationStack {
             ScrollView {
@@ -49,7 +55,7 @@ struct DashboardView: View {
                                         .font(.title3.bold())
                                         .foregroundStyle(.pink)
                                     
-                                    Text("Avg: 10K Steps")
+                                    Text("Avg: \(Int(avgStepCount)) Steps")
                                         .font(.caption)
                                 }
                                 
@@ -61,13 +67,33 @@ struct DashboardView: View {
                         .foregroundStyle(.secondary)
                         .padding(.bottom, 12)
                         
-                        Chart(hkManager.stepData) { steps in
-                            BarMark(
-                                x: .value("Date", steps.date, unit: .day),
-                                y: .value("Steps", steps.value)
-                            )
+                        Chart {
+                            RuleMark(y: .value("Average", avgStepCount))
+                                .foregroundStyle(Color.secondary)
+                                .lineStyle(.init(lineWidth: 1, dash: [5]))
+                            
+                            ForEach(hkManager.stepData) { steps in
+                                BarMark(
+                                    x: .value("Date", steps.date, unit: .day),
+                                    y: .value("Steps", steps.value)
+                                )
+                                .foregroundStyle(.pink.gradient)
+                            }
                         }
                         .frame(height: 150)
+                        .chartXAxis {
+                            AxisMarks {
+                                AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
+                            }
+                        }
+                        .chartYAxis {
+                            AxisMarks { value in
+                                AxisGridLine()
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                
+                                AxisValueLabel((value.as(Double.self) ?? 0).formatted(.number.notation(.compactName)))
+                            }
+                        }
                     }
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
--- Step Tracker/HealthKitManager.swift
+++ Step Tracker/HealthKitManager.swift
@@ -28,10 +28,13 @@ import HealthKit
                                                                anchorDate: endDate,
                                                                intervalComponents: .init(day: 1))
         
-        let stepCounts = try! await stepsQuery.result(for: store)
-        
-        stepData = stepCounts.statistics().map {
-            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        do {
+            let stepCounts = try await stepsQuery.result(for: store)
+            stepData = stepCounts.statistics().map {
+                .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+            }
+        } catch {
+            
         }
     }
     
@@ -47,11 +50,13 @@ import HealthKit
                                                                options: .mostRecent,
                                                                anchorDate: endDate,
                                                                intervalComponents: .init(day: 1))
-        
-        let weights = try! await weightQuery.result(for: store)
-        
-        weightData = weights.statistics().map {
-            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+        do {
+            let weights = try await weightQuery.result(for: store)
+            weightData = weights.statistics().map {
+                .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .pound()) ?? 0)
+            }
+        } catch {
+            
         }
     }
     
